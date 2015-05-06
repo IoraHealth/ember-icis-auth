786418f99fc80c41bb09302a196eecafea20f95c
Myke Cameron <mykecameron@gmail.com>
2015-05-06 13:31:23 -0400

    don't use model to fetch data
    this way this mixin can be used in routes which want to load route specific data in the model hooks.

--- addon/mixins/authenticated-route-mixin.js
+++ addon/mixins/authenticated-route-mixin.js
@@ -7,25 +7,34 @@ export default Ember.Mixin.create({
   practice_id: localStorage['practice_id'],
 
   beforeModel: function(transition) {
-    var superResult = this._super(transition);
+    this._super(transition);
 
     var token = this.get('token');
     if(!token) {
       this._authenticate();
+    } else {
+      var _this = this;
+
+      return this.store.find('current-user', 'me').then(
+
+        function(currentUser) {
+          _this.set('user', currentUser);
+          _this.set('practiceUser', _this._getPracticeUser(currentUser, _this.get('practice_id')));
+
+          return Ember.RSVP.resolve(currentUser);
+        }
+      );
     }
-
-    return superResult;
-  },
-
-  model: function() {
-    return this.store.find('current-user', 'me');
   },
 
   setupController: function(controller, model) {
-    this._super(controller, model);
-    this.set('user', model);
-    this.set('practiceUser', this._getPracticeUser(model, this.get('practice_id')));
-    controller.set('practiceUser', this._getPracticeUser(model, this.get('practice_id')));
+    var superResult = this._super(controller, model);
+
+    var currentUser  = this.get('user');
+    var practiceUser = this._getPracticeUser(currentUser, this.get('practice_id'));
+    controller.set('practiceUser', practiceUser);
+
+    return superResult;
   },
 
   _authenticate: function() {
