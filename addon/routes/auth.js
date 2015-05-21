3db3ea3f499132541ed085f9dd68a4ffae5f8a64
Myke Cameron <mykecameron@gmail.com>
2015-05-21 11:26:09 -0400

    break transitioning after auth into overridable method

--- addon/routes/auth.js
+++ addon/routes/auth.js
@@ -3,12 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   snowflake_provider: "CHANGEME",
 
-  setupController: function() {
+  beforeModel: function(transition) {
     var _this = this;
 
     OAuth.callback(this.get('snowflake_provider')).done(function(result) {
       localStorage['access_token'] = result.access_token;
-      _this.transitionTo('index');
+      _this.transitionToTargetRoute(transition);
     });
+  },
+
+  transitionToTargetRoute: function(transition) {
+    this.transitionTo('index');
   }
 });
