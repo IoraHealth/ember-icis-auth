ef2fb693b191b363bc29a3948bc0c40f522ff491
Matt Doller <matt.doller@iorahealth.com>
2016-01-25 15:19:28 -0500

    Add currentPracticeUser to currentUser

--- addon/models/current-user.js
+++ addon/models/current-user.js
@@ -4,10 +4,17 @@ export default DS.Model.extend({
   uid: DS.attr('string'),
   currentPracticeUsers: DS.hasMany('currentPracticeUser', { async: false }),
   access_roles: DS.attr(),
+  current_practice_user_uid: DS.attr('string'),
 
   practiceUser: function(uid) {
     return this.get('currentPracticeUsers').findBy('id', uid);
   },
+
+  currentPracticeUser: function() {
+    let uid = this.get('current_practice_user_uid');
+
+    return this.practiceUser(uid);
+  }.property('current_practice_user_uid'),
 
   practiceUserByPracticeId: function(practiceId) {
     return this.get('currentPracticeUsers').findBy('practiceId', parseInt(practiceId, 10));
