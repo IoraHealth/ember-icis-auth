55d818f1d5c67b07ed407505e9e345fbc260579a
Trent Ogren <tedwardo2@gmail.com>
2016-03-02 09:27:00 -0600

    Update authenticated-route-mixin.js

--- addon/mixins/authenticated-route-mixin.js
+++ addon/mixins/authenticated-route-mixin.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  token: localStorage['access_token'],
+  token: Ember.computed({
+    get() {
+      return localStorage['access_token'];
+    },
+    set(_, value) {
+      localStorage['access_token'] = value;
+      return value;
+    }
+  }),
   snowflake_provider: 'CHANGEME',
   snowflake_url: 'CHANGEME',
 
@@ -9,8 +17,7 @@ export default Ember.Mixin.create({
     this._super(transition);
 
     if (transition.queryParams && transition.queryParams.access_token) {
-      localStorage['access_token'] = transition.queryParams.access_token;
-      this.set('token', localStorage['access_token']);
+      this.set('token', transition.queryParams.access_token);
     }
 
     if(!this.get('token')) {
