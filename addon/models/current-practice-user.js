a92d8ca02f99a7cde82f8c995daf9dc6d9fd3035
Garrett Dycus <garrett.dycus@iorahealth.com>
2016-04-18 16:11:48 -0400

    Add signable attribute to current practice user

--- addon/models/current-practice-user.js
+++ addon/models/current-practice-user.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   default: DS.attr('boolean'),
   practiceId: DS.attr('number'),
   role: DS.attr('string'),
+  signable: DS.attr('boolean'),
 
   isProvider: function() {
     return this.get('role') === 'Provider';
